849313649a812358c89a84c72d31a5e77aba127b
Yukkitorie <manastormmillhaus@gmail.com>
2025-01-21 14:47:49 +0000

    minor_update

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -4,19 +4,19 @@
 <div>
   <form action="{{ route('post.store') }}" method="POST">
     @csrf
-    <div>
+    <div class="px-2">
       <label for="title">Title</label>
       <input class="form-control" name="title"  id="title" type="text" placeholder="type smth...">
     </div>
-    <div>
+    <div class="px-2">
       <label for="content">Content</label>
       <textarea class="form-control" name="content"  id="content" placeholder="type smth..."></textarea>
     </div>
-    <div>
+    <div class="px-2">
       <label for="image">Image</label>
       <input class="form-control" name="image"  id="image" type="text" placeholder="type smth...">
     </div>
-    <div>
+    <div class="px-2">
       <label for="btn"></label>
       <button type="submit"name=""   id="btn" class="btn btn-primary">Create</button>
     </div>
